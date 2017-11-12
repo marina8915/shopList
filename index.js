040e5c25d43398c628bbdbbf9d710ae247cf9eb3
marina <marina_bakum@mail.ru>
2017-11-12 11:15:53 +0200

    deleted id checkbox, fixed bag Change()

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function onLoadFunct() {
     //event for button submit
     var form = document.getElementById('form')
@@ -20,7 +19,7 @@ function onLoadFunct() {
             var value = document.getElementById('item').value
             var empty = -value
             if (empty !== -0) {
-                el.innerHTML = '<input type="checkbox" id="check' + n + '"  onchange="Check()">' + value + del
+                el.innerHTML = '<input type="checkbox" onchange="Check()">' + value + del
                 list.insertBefore(el, list.childNodes[0])
             }
             //reset input
@@ -51,20 +50,14 @@ function onLoadFunct() {
             Array.prototype.forEach.call(liList, function () {
                 //find selected element
                 for (i = 0; i < liList.length; i++) {
-                    var idLi = liList[i].getAttribute('id')
-                    var idCheck = 'check' + idLi
-                    var li = document.getElementById(idCheck)
-                    if (li.checked) {
+                    if (liList[i].querySelector('input').checked) {
                         selectedList.appendChild(liList[i])
                     }
                 }
                 //find unselected element in select
                 el = selectedList.getElementsByTagName('li')
                 for (i = 0; i < el.length; i++) {
-                    idLi = el[i].getAttribute('id')
-                    idCheck = 'check' + idLi
-                    li = document.getElementById(idCheck)
-                    if (li.checked === false) {
+                    if (el[i].querySelector('input').checked === false) {
                         unselectedList.appendChild(el[i])
                     }
                 }
@@ -82,20 +75,14 @@ function onLoadFunct() {
             Array.prototype.forEach.call(liList, function () {
                 //find unselected element
                 for (i = 0; i < liList.length; i++) {
-                    idLi = liList[i].getAttribute('id')
-                    idCheck = 'check' + idLi
-                    li = document.getElementById(idCheck)
-                    if (li.checked === false) {
+                    if (liList[i].querySelector('input').checked === false) {
                         unselectedList.appendChild(liList[i])
                     }
                 }
                 //find selected element in unselected
                 el = unselectedList.getElementsByTagName('li')
                 for (i = 0; i < el.length; i++) {
-                    idLi = el[i].getAttribute('id')
-                    idCheck = 'check' + idLi
-                    li = document.getElementById(idCheck)
-                    if (li.checked) {
+                    if (el[i].querySelector('input').checked) {
                         selectedList.appendChild(el[i])
                     }
                 }
@@ -106,21 +93,23 @@ function onLoadFunct() {
         })
     }
 }
+
 //delete item
 function Del(id) {
     var el = document.getElementById(id)
     el.parentNode.removeChild(el)
+    Check()
 }
+
 //sum unchecked item
 function Check() {
-    Array.prototype.forEach.call(document.getElementsByTagName('li'), function () {
-        var item = 0
-        var el = document.getElementsByTagName('li')
-        for (var i = 0; i < el.length; i++) {
-            if (el[i].querySelector('input').checked === false) {
-                item+=1
-                document.getElementById('sum').innerHTML = 'need to buy ' + item + ' items '
-            }
+    var el = document.getElementsByTagName('li')
+    var item = 0
+    document.getElementById('sum').innerHTML = ''
+    for (var i = 0; i < el.length; i++) {
+        if (el[i].querySelector('input').checked === false) {
+            item += 1
+            document.getElementById('sum').innerHTML = 'need to buy ' + item + ' items '
         }
-    }, false)
+    }
 }
